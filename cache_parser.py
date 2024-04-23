439b4234abf307126ccdefd6c68cfd705b0e0f3a
jamesratzlaff <jamesratzlaff@gmail.com>
2024-04-23 03:22:36 -0400

    implemented Path object based loading

--- cache_parser.py
+++ cache_parser.py
@@ -1,10 +1,25 @@
 # originally from https://gist.github.com/fxtentacle/d3002f2c4084b9758c4228d5aedb276a
 import numpy as np
 import json
+from pathlib import Path
+import argparse
 
-base_path="."
-param_dir="param"
-cache_dir="cache"
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('-p','--param_dir', help="the name of the 'param' dir relative to the basepath",default='param')
+arg_parser.add_argument('-c','--cache_dir', help="the name of the 'cache' dir relative to the basepath", default='cache')
+arg_parser.add_argument('basepath',default=".",nargs='?', help="the path containing the property.rvproj file")
+args = arg_parser.parse_args()
+print(args)
+
+base_path_val=args.basepath
+param_dir_val=args.param_dir
+cache_dir_val=args.cache_dir
+
+basepath=Path(base_path_val)
+param_dir=Path(basepath,param_dir_val)
+cache_dir=Path(basepath,cache_dir_val)
+
+print(param_dir.absolute())
 
 def from_json(json,*keys):
     current = json
@@ -29,7 +44,7 @@ def from_json_or_default(json,*vals,default=None):
     return reso
 
 # load the project JSON to get the depth scale
-with open('./property.rvproj','rt') as f:
+with Path(basepath,'property.rvproj').absolute().open(mode='rt') as f:
     project = json.load(f)
 depth_scale = project['scan_param']['depth_scale']
 depth_height = from_json_or_default(project,['scan_param','depth_height'],['scan_param','ir_height'],default=400)
@@ -37,14 +52,15 @@ depth_width = from_json_or_default(project,['scan_param','depth_width'],['scan_p
 print(depth_scale)
 
 # load projection matrix from 2D to 3D
-Q = np.fromfile('./param/Q.bin', count=4*4, dtype=np.float32).reshape((4,4,))
+Q = np.fromfile(str(Path(param_dir,'Q.bin').absolute()), count=4*4, dtype=np.float32).reshape((4,4,))
 print('Q', Q)
 
 
 
 def load_frame(filename):
+    fname=str(filename)
     # load depth map, convert to float, scale
-    frame = np.fromfile(filename, dtype=np.uint16).reshape((depth_height,depth_width,)).astype(np.float32) * depth_scale
+    frame = np.fromfile(fname, dtype=np.uint16).reshape((depth_height,depth_width,)).astype(np.float32) * depth_scale
 
     # make [x,y,z,1] vectors
     xs = np.arange(0,depth_width).astype(np.float32)
@@ -70,7 +86,7 @@ def load_frame(filename):
 
     if True:
         # load the transformation matrix for this frame
-        T = np.fromfile(filename.replace('.dph','.inf'), offset=0x10, count=4*4, dtype=np.float64).reshape((4, 4,))
+        T = np.fromfile(fname.replace('.dph','.inf'), offset=0x10, count=4*4, dtype=np.float64).reshape((4, 4,))
         print('T', T)
         # convert point cloud to [x,y,z,1] and multipla and reduce to [x/w,y/w,z/w]
         data = np.concatenate([data,np.ones_like(data[:,0:1])],-1)
@@ -79,25 +95,30 @@ def load_frame(filename):
 
     return data
 
+depth_files = cache_dir.absolute().glob("*.dph")
+
 # load demo frames and merge them into one point cloud
-data = np.concatenate([
-    load_frame('./cache/frame_000_0000.dph'),
-    load_frame('./cache/frame_000_0002.dph'),
-    load_frame('./cache/frame_000_0004.dph'),
-    load_frame('./cache/frame_000_0006.dph'),
-    load_frame('./cache/frame_000_0008.dph'),
-    load_frame('./cache/frame_000_0010.dph'),
-    load_frame('./cache/frame_000_0012.dph'),
-    load_frame('./cache/frame_000_0014.dph'),
-    load_frame('./cache/frame_003_0000.dph'),
-    load_frame('./cache/frame_003_0002.dph'),
-    load_frame('./cache/frame_003_0004.dph'),
-    load_frame('./cache/frame_003_0020.dph'),
-    load_frame('./cache/frame_004_0001.dph'),
-    load_frame('./cache/frame_004_0003.dph'),
-    load_frame('./cache/frame_008_0001.dph'),
-    load_frame('./cache/frame_008_0107.dph'),
-], 0)
+data = np.concatenate(
+    list(map(load_frame,depth_files)),
+#     [
+#     load_frame('./cache/frame_000_0000.dph'),
+#     load_frame('./cache/frame_000_0002.dph'),
+#     load_frame('./cache/frame_000_0004.dph'),
+#     load_frame('./cache/frame_000_0006.dph'),
+#     load_frame('./cache/frame_000_0008.dph'),
+#     load_frame('./cache/frame_000_0010.dph'),
+#     load_frame('./cache/frame_000_0012.dph'),
+#     load_frame('./cache/frame_000_0014.dph'),
+#     load_frame('./cache/frame_003_0000.dph'),
+#     load_frame('./cache/frame_003_0002.dph'),
+#     load_frame('./cache/frame_003_0004.dph'),
+#     load_frame('./cache/frame_003_0020.dph'),
+#     load_frame('./cache/frame_004_0001.dph'),
+#     load_frame('./cache/frame_004_0003.dph'),
+#     load_frame('./cache/frame_008_0001.dph'),
+#     load_frame('./cache/frame_008_0107.dph'),
+# ], 
+0)
 
 # display result
 import open3d as o3d
